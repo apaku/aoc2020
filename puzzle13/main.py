00ccf81b02b2026ea648aeae9cb0dec38e0167eb
Andreas Pakulat <andreaspakulat@gmail.com>
2020-12-13 12:37:18 +0100

    Fast version of part2
    Instead of all multiples of one of the busses, check multiples of the
    timestamp where the already-handled busses 'meet'. So for
    7,13,x,x,59,x,31,19 the first timestamp is 77, since 78 mod 13 is 0.
    That means when finding a timestamp where 7, 13 and 59 line up we can go
    with multiples of 77. In turn for 31 lining up in addition it's possible
    to go with multiples of 350. The basic idea is that the line-up of n
    busses repeats until the n+1th bus also matches up.
    
    This reduces the number of iterations greatly, since the 'stepsize' is
    quickly increasing.

--- puzzle13/main.py
+++ puzzle13/main.py
@@ -16,17 +16,17 @@ if len(lines) == 2:
 else:
     busses = lines[0].split(",")
 
-busses_part2 = list(reversed(sorted(filter(lambda x: x[1] != 0, enumerate(map(lambda b: 0 if b == 'x' else int(b), busses))), key=lambda b: b[1])))
-print("Part2: {}".format(busses_part2))
-multiple = 1
-while True:
-    t = multiple * busses_part2[0][1] - busses_part2[0][0]
-    nonmatching = False
-    for bus in busses_part2[1:]:
-        if ( t + bus[0] ) % bus[1] != 0:
-            nonmatching = True
-            break
-    if not nonmatching:
-        print("Part2: {}".format(t))
-        break
-    multiple += 1
+busses_part2 = list(filter(lambda x: x[1] != 0, enumerate(map(lambda b: 0 if b == 'x' else int(b), busses))))
+#busses_part2 = list(reversed(sorted(filter(lambda x: x[1] != 0, enumerate(map(lambda b: 0 if b == 'x' else int(b), busses))), key=lambda b: b[1])))
+#print("Part2: {}".format(busses_part2))
+#multiple = 100000000000000 // busses_part2[0][1]
+#print("multiple: {} factor: {}".format(multiple, busses_part2[0]))
+target = 0
+stepsize = 1
+for (idx, bus) in busses_part2:
+    print("Handling bus: {}/{} with target: {}".format(idx,bus, target))
+    while (target+idx) % bus != 0:
+        target += stepsize
+    stepsize *= bus
+    print("target after bus {}/{}: {}, n: {}".format(idx, bus, target, stepsize))
+print("Part2: {}".format(target))
