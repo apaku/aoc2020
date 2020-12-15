6b12f1bcf9c6bc0954afb9f7311e7509ae3bd581
Andreas Pakulat <andreas@froglogic.com>
2020-12-15 07:08:57 +0100

    solution to part 2 day14
    Having a fresh thought about this helped, wasn't that hard at all.

--- puzzle14/main.py
+++ puzzle14/main.py
@@ -3,6 +3,7 @@ import sys
 
 mask = None
 memory = {}
+instructions = sys.stdin.readlines()
 
 def apply_mask(value, mask_as_str):
     bin_str = f'{value:036b}'
@@ -14,7 +15,7 @@ def apply_mask(value, mask_as_str):
             target.append(c)
     return int(''.join(target), 2)
 
-for instr in sys.stdin.readlines():
+for instr in instructions:
     if instr[:3] == 'mem':
         value = instr[instr.index(" = ")+3:]
         addr = instr[4:instr.index("]")]
@@ -23,3 +24,35 @@ for instr in sys.stdin.readlines():
         mask = instr.strip().replace("mask = ", "")
 
 print("Part1: {}".format(sum(memory.values())))
+
+memory = {}
+mask = None
+
+def apply_mask_part2(value, mask_as_str):
+    bin_str = f'{value:036b}'
+    target = [[]]
+    for (i,c) in enumerate(mask_as_str):
+        if c == '0':
+            for t in target:
+                t.append(bin_str[i])
+        elif c == '1':
+            for t in target:
+                t.append("1")
+        elif c == 'X':
+            newtargets = []
+            for t in target:
+                newtargets.append(t + ["0"])
+                t.append("1")
+            target += newtargets
+    for t in target:
+        yield int("".join(t), 2)
+
+for instr in instructions:
+    if instr[:3] == 'mem':
+        value = instr[instr.index(" = ")+3:]
+        for addr in apply_mask_part2(int(instr[4:instr.index("]")]), mask):
+            memory[addr] = int(value)
+    else:
+        mask = instr.strip().replace("mask = ", "")
+
+print("Part2: {}".format(sum(memory.values())))
