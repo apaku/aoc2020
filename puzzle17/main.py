18a0676fc8d35c510c24350d9c939f4029bcefa7
Andreas Pakulat <andreas@froglogic.com>
2020-12-18 15:25:20 +0100

    Day 17 finished
    After starting to visualize the cubes in each iteration I found the
    off-by-one error causing me grieve before.
    
    Since part 2 just adds one dimension I ended up rewriting the code
    to support n-dimensional input and use part1 to test that rewrite.

--- puzzle17/main.py
+++ puzzle17/main.py
@@ -6,25 +6,52 @@ def count_gen(generator):
 
 def count_active_neighbors(cube_coord, cubes):
     def generate_neighbors():
-        for direction in filter(lambda x: x != (0,0,0), product((-1,0,1),(-1,0,1),(-1,0,1))):
-            yield (cube_coord[0]+direction[0], cube_coord[1]+direction[1], cube_coord[2]+direction[2])
+        axis_count = len(list(cubes.keys())[0])
+        all_directions = product(*[(-1,0,1) for axis in range(axis_count)])
+        for direction in filter(lambda x: x != axis_count * (0,), all_directions):
+            cube = tuple([cube_coord[axis] + direction[axis] for axis in range(axis_count)])
+            yield cube
     return count_gen(filter(lambda n: n in cubes and cubes[n], generate_neighbors()))
 
 def visualize(cubes):
-    min_max = (min(map(lambda c:c[0], cubes.keys())), max(map(lambda c:c[0], cubes.keys())))
-    zrange = (min(map(lambda c:c[2], cubes.keys())), max(map(lambda c:c[2], cubes.keys())))
-    print("z: {}: min/max: {}".format(zrange, min_max))
-    for z in range(zrange[0], zrange[1]+1):
-        print("z = {}".format(z))
+    additional_ranges = []
+    x_y_range = (min(map(lambda c: c[0], cubes.keys())), max(map(lambda c: c[0], cubes.keys())))
+    for axis in range(2, len(list(cubes.keys())[0])):
+        additional_ranges.append(list(range(min(map(lambda c: c[axis], cubes.keys())), max(map(lambda c: c[axis], cubes.keys()))+1)))
+    print("Additional axes: {}".format(additional_ranges))
+    additional_axes_combos = product(*additional_ranges)
+    for axis_combo in additional_axes_combos:
+        print("{}".format(axis_combo))
         layer = []
-        for y in range(min_max[0], min_max[1]+1):
+        for y in range(x_y_range[0], x_y_range[1]+1):
             row = []
-            for x in range(min_max[0], min_max[1]+1):
-                cube = (x,y,z)
-                row.append("#" if (x,y,z) in cubes and cubes[(x,y,z)] else ".")
+            for x in range(x_y_range[0], x_y_range[1]+1):
+                cube = tuple([x,y] + list(axis_combo))
+                row.append("#" if cube in cubes and cubes[cube] else ".")
             layer.append("".join(row))
         print("\n".join(layer))
         print("\n")
+
+def simulate(start):
+    cubes = start
+    axis_count = len(list(cubes.keys())[0])
+    for c in range(6):
+        new_cubes = {}
+        axis_ranges = []
+        for axis in range(axis_count):
+            axis_ranges.append(list(range(min(map(lambda c: c[axis], cubes.keys())) - 1, max(map(lambda c: c[axis], cubes.keys()))+2)))
+        all_cubes = product(*axis_ranges)
+        for cube in all_cubes:
+            active_neighbors = count_active_neighbors(cube, cubes)
+            if cube in cubes and cubes[cube]:
+                new_cubes[cube] = 2 <= active_neighbors <= 3
+            else:
+                new_cubes[cube] = active_neighbors == 3
+
+        visualize(new_cubes)
+        print(" ======= ======= =======")
+        cubes = new_cubes
+    return cubes
 
 cubes = {}
 
@@ -40,27 +67,22 @@ for c in sys.stdin.read():
         cubes[(x,y,z)] = False
     x += 1
 
+initial_cubes = cubes
+
+print("PART1")
+visualize(cubes)
+
+cubes = simulate(cubes)
+
+print("Part1: {}".format(count_gen(filter(lambda c: c[1], cubes.items()))))
+
+print("PART2")
+cubes = {}
+for c,v in initial_cubes.items():
+    cubes[(c[0], c[1], c[2], 0)] = v
 
 visualize(cubes)
-print("START")
 
-for c in range(6):
-    new_cubes = {}
-    for x in range(min(map(lambda c: c[0], cubes.keys())) - 1, max(map(lambda c: c[0], cubes.keys()))+1):
-        for y in range(min(map(lambda c: c[1], cubes.keys()))-1, max(map(lambda c: c[1], cubes.keys()))+1):
-            for z in range(min(map(lambda c: c[2], cubes.keys()))-1, max(map(lambda c: c[2], cubes.keys()))+1):
-                cube = (x,y,z)
-                active_neighbors = count_active_neighbors(cube, cubes)
-                print("neighbors of {}: {}".format(cube, active_neighbors))
-                if cube in cubes and cubes[cube]:
-                    print("Setting active {} with {} to {}".format(cube, active_neighbors, 2 <= active_neighbors <= 3))
-                    new_cubes[cube] = 2 <= active_neighbors <= 3
-                else:
-                    print("Setting inactive {} with {} to {}".format(cube, active_neighbors, active_neighbors == 3))
-                    new_cubes[cube] = active_neighbors == 3
-    visualize(new_cubes)
-    print(" ======= ======= =======")
-    cubes = new_cubes
+cubes = simulate(cubes)
+print("Part2: {}".format(count_gen(filter(lambda c: c[1], cubes.items()))))
 
-print("Part1: {}".format(list(filter(lambda c: c, cubes.values()))))
-print("Part1: {}".format(count_gen(filter(lambda c: c[1], cubes.items()))))
